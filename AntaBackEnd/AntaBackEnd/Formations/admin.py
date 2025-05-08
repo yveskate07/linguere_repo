7f83f6f4909cf650349d9f8d5170903876485a5e
yveskate07 <kateyveschadrac@gmail.com>
2025-05-08 16:45:13 +0000

    Backend Formation terminé à 96%

---
+++ AntaBackEnd/AntaBackEnd/Formations/admin.py
@@ -0,0 +1,136 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+from Formations.models import *
+
+
+admin.site.unregister(Group)
+
+# Register your models here.
+@admin.register(Formations)
+class FormationAdmin(admin.ModelAdmin):
+    fields = ('name','motiv','duration', 'description','image','determinant','availability','hours_per_week',)
+    readonly_fields = ('description',)
+    list_display = ('name','get_duration_display_fr','hours_per_week','availability',)
+    search_fields = ('name',)
+    list_filter = ('availability',)
+
+    def get_duration_display_fr(self, obj):
+        return obj.get_duration_display_fr()
+
+    get_duration_display_fr.short_description = 'Durée'
+
+
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    fields = ('name','formation', 'description',)
+    search_fields = ('name',)
+    list_filter = ('formation',)
+    list_display = ('name', 'formation',)
+    readonly_fields = ('description',)
+
+    """def has_add_permission(self, request):
+        return False"""
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(Prerequisites)
+class PrerequisitesAdmin(admin.ModelAdmin):
+    fields = ('name','level','image','formation',)
+    list_display = ('name', 'level','formation',)
+    search_fields = ('name',)
+    list_filter = ('level','formation',)
+
+    """def has_add_permission(self, request):
+        return False"""
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(SkillGained)
+class SkillGainedAdmin(admin.ModelAdmin):
+    fields = ('name','description_skill','formation',)
+    list_filter = ('formation',)
+    list_display = ('name','description_skill','formation',)
+
+    """def has_add_permission(self, request):
+        return False"""
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(MotivPoints)
+class MotivPointsAdmin(admin.ModelAdmin):
+    fields = ('name','description','formation',)
+    list_filter = ('formation',)
+    list_display = ('name', 'description','formation',)
+
+    """def has_add_permission(self, request):
+        return False"""
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(Advantages)
+class AdvantagesAdmin(admin.ModelAdmin):
+    fields = ('name', 'description','formation',)
+    list_filter = ('formation',)
+    list_display = ('name', 'description','formation',)
+
+    """def has_add_permission(self, request):
+        return False"""
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+@admin.register(Testimony)
+class TestimonyAdmin(admin.ModelAdmin):
+    fields = ('formation','username','status','comment', 'description',)
+    list_display = ('formation','username','status','comment',)
+    readonly_fields = ('description',)
+
+    """def has_add_permission(self, request):
+        return False"""
+
+@admin.register(SignedUpUser)
+class SignedUpUserAdmin(admin.ModelAdmin):
+    fields = ('name','email','tel_number','availability','session','formation', 'description',)
+    list_filter = ('formation','session','availability',)
+    search_fields = ('name',)
+    readonly_fields = ('description',)
+
+    list_display = ('name','email','tel_number','availability','session','formation')
+
+    """def has_add_permission(self, request):
+        return False"""
+
+@admin.register(UserBrochure)
+class UserBrochureAdmin(admin.ModelAdmin):
+    fields = ('name','email','availability','formation', 'description',)
+    list_display = ('name','email','availability','formation',)
+    search_fields = ('name',)
+    list_filter = ('availability','formation',)
+    readonly_fields = ('description',)
+
+    """def has_add_permission(self, request):
+        return False"""
+
+@admin.register(UserRequest)
+class UserRequestAdmin(admin.ModelAdmin):
+    fields = ('name','email','formation', 'description',)
+    list_display = ('name', 'email', 'formation',)
+    search_fields = ('name',)
+    list_filter = ('formation',)
+    readonly_fields = ('description',)
+
+    """def has_add_permission(self, request):
+        return False"""
---
+++ AntaBackEnd/AntaBackEnd/Formations/forms.py
@@ -0,0 +1,116 @@
+from django import forms
+from .models import SignedUpUser, UserBrochure, UserRequest
+
+
+class SignedUpUserForm(forms.ModelForm):
+    class Meta:
+        model = SignedUpUser
+        fields = ['name', 'email', 'tel_number', 'availability', 'session', 'message']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'placeholder': 'Entrez votre nom complet', 'id': 'inscription-name', 'name': 'inscription-name'}),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500'
+                , 'placeholder': 'Entrez votre adresse e-mail', 'id': 'inscription-email',
+        }),
+
+            'tel_number': forms.TextInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'placeholder': 'Entrez votre numéro de téléphone', 'id': "inscription-phone",
+        }),
+
+            'availability': forms.Select(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'id': "inscription-method"}),
+
+            'session': forms.Select(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'id': "inscription-session"}),
+
+            'message': forms.Textarea(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'id': "inscription-message", 'rows': 3,
+                'placeholder': 'Avez-vous des questions ou des besoins spécifiques ?'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        email = cleaned_data.get('email')
+
+        if name:
+            cleaned_data['name'] = name.strip()
+        if email:
+            cleaned_data['email'] = email.strip()
+
+        if cleaned_data.get('name') and cleaned_data.get('email'):
+            if SignedUpUser.objects.filter(
+                name__iexact=cleaned_data['name'],
+                email__iexact=cleaned_data['email']
+            ).exists():
+                print("Un utilisateur avec ce nom et cet email est déjà inscrit.")
+                raise forms.ValidationError(
+                    "Un utilisateur avec ce nom et cet email est déjà inscrit."
+                )
+
+            else:
+                print("Enregistrement d'un nouveau client")
+
+        return cleaned_data
+
+
+class BrochureForm(forms.ModelForm):
+    class Meta:
+        model = UserBrochure
+        fields = ['name', 'email', 'tel_number', 'availability', 'message']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'placeholder': 'Entrez votre nom complet', 'id': 'name', 'name': 'name'}),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500'
+                , 'placeholder': 'Entrez votre adresse e-mail', 'id': 'email'}),
+
+            'tel_number': forms.TextInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'placeholder': 'Entrez votre numéro de téléphone', 'id': "phone"}),
+
+            'availability': forms.Select(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'id': "method"}),
+
+            'message': forms.Textarea(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'id': "message", 'rows': 3,
+                'placeholder': 'Avez-vous des questions ou des besoins spécifiques ?'})
+        }
+
+
+class RequestForm(forms.ModelForm):
+    class Meta:
+        model = UserRequest
+        fields = ['name', 'email', 'tel_number', 'message']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'placeholder': 'Entrez votre nom complet', 'id': 'contact-name'}),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500'
+                , 'placeholder': 'Entrez votre adresse e-mail', 'id': 'contact-email'}),
+
+            'tel_number': forms.TextInput(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'placeholder': 'Entrez votre numéro de téléphone', 'id': "contact-phone"}),
+
+            'message': forms.Textarea(attrs={
+                'class': 'w-full border border-gray-300 rounded-lg p-2 mt-1 focus:ring-indigo-500 focus:border-indigo-500',
+                'id': "contact-message", 'rows': 3,
+                'placeholder': 'Avez-vous des questions ou des besoins spécifiques ?'})
+        }
