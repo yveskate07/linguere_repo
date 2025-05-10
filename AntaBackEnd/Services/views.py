dd438bc64d20b0a4ceb3d390c216038c3cb2f26f
desire427 <alphonsedesirehaba427@gmail.com>
2025-05-10 10:49:59 +0000

    service modifer

--- AntaBackEnd/Services/views.py
+++ AntaBackEnd/Services/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def broderie_numerique(request):
+def broderie_numerique(request): 
     return render(request, "Services/broderie/index.html")
 
 def fraiseuse_numerique(request):
@@ -11,7 +11,7 @@ def decoupe_laser(request):
     return render(request, "Services/laser/index.html")
 
 def impression_3D(request):
-    return render(request, 'Services/')
+    return render(request, 'Services/Impre_3D/index.html')
 
 def impression_num_proposees(request):
-    return render(request, 'Services/')
+    return render(request, 'Services/Impre_Num/index.html')
