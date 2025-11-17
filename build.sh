732165db3a73cd4d2242011db9294bf03f24dd68
yveskate07 <kateyveschadrac@gmail.com>
2025-11-17 00:20:13 +0000

    Ajout du fichier requirements.txt. Prochaine etape: Render

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+# Make sure the script is executable before adding it to version control:
+# chmod a+x build.sh
