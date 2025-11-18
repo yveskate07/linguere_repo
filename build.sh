f7b4212f7ce00f1bee976be26ad018f7e198466e
yveskate07 <kateyveschadrac@gmail.com>
2025-11-18 09:27:58 +0000

    build.sh file updated

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "updating system packages and installing wkhtmltopdf..."
 apt-get update && apt-get install -y wkhtmltopdf
 
+echo "setting up python environment by installing requirements..."
 pip install -r requirements.txt
 
+echo "collecting static files..."
 python manage.py collectstatic --no-input
 
+echo "applying database migrations..."
 python manage.py makemigrations
 
 python manage.py migrate
