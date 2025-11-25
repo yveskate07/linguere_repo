1e5152e41568b1bcca536b4ba81b1d04d04cd3c9
yveskate07 <kateyveschadrac@gmail.com>
2025-11-25 02:46:46 +0000

    Auto-commit on 2025-11-25 02:46:46

--- Activities/apps.py
+++ Activities/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ActivitiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Activities'
+    verbose_name = 'Projets & Activités'
--- Services/apps.py
+++ Services/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ServicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Services'
+    verbose_name = 'Services & Customisation'
