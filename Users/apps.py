5fe5969d73a9abde905666d3dfe32bb2bcc1c284
yveskate07 <kateyveschadrac@gmail.com>
2025-12-12 17:40:55 +0000

    Commit on 2025-12-12 17:40:55, New permissions added.

--- Users/apps.py
+++ Users/apps.py
@@ -1,10 +1,9 @@
 from django.apps import AppConfig
-print("signals.py bien chargé")
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Users'
     verbose_name = 'Utilisateurs'
 
-def ready(self):
-    from Users import signals
+    def ready(self):
+        from Users import signals
