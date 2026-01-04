221b9d389d3e8baa7a1ab84c26963c811416a878
yveskate07 <kateyveschadrac@gmail.com>
2026-01-04 23:40:08 +0000

    Commit on 2026-01-04 23:40:08, Shop articles added on build.

--- create_superuser.py
+++ create_superuser.py
@@ -7,32 +7,35 @@ from django.db import connection
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AntaBackEnd.settings")
 django.setup()
 
-
 # displaying if wether we are using a sqlite3 database or postgresql database
 #print(f"database used is {settings.DATABASES}")
 
 User = get_user_model()
 
 
-if not User.objects.filter(username="yveskate07", is_superuser=True).exists():
+try:
+    User.objects.get(username="yveskate07", is_superuser=True)
     """print("Truncating the table Users_fab_user...")
     # because we have sometimes this error : django.db.utils.IntegrityError: duplicate key value violates unique constraint "Users_fab_user_pkey"
     # we need to truncate the table of the users
     with connection.cursor() as cursor:
         cursor.execute('TRUNCATE TABLE "Users_fab_user" RESTART IDENTITY CASCADE;')
     print("Superuser n'existe pas.")"""
+except User.DoesNotExist:
     User.objects.create_superuser(first_name="yves", last_name="kate",username="yveskate07" ,tel_num="+221781586866" ,adress="Senegal" , email="kateyveschadrac@gmail.com", password="azerty12345")
     print("Superuser créé !")
 else:
     print("Superuser existe déjà.")
 
-if not User.objects.filter(username="linguerefablab221", is_superuser=False).exists():
+try:
+    User.objects.get(username="linguerefablab221", is_superuser=False)
     """print("Truncating the table Users_fab_user...")
     # because we have sometimes this error : django.db.utils.IntegrityError: duplicate key value violates unique constraint "Users_fab_user_pkey"
     # we need to truncate the table of the users
     with connection.cursor() as cursor:
         cursor.execute('TRUNCATE TABLE "Users_fab_user" RESTART IDENTITY CASCADE;')
     print("Superuser n'existe pas.")"""
+except User.DoesNotExist:
     User.objects.create_superuser(first_name="Anta", last_name="Ngom",username="linguerefablab221" ,tel_num="+221773146662" ,adress="Senegal" , email="linguerefablab@gmail.com", password="azerty12345")
     print("Linguere user créé !")
 else:
