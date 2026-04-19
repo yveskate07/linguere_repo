5446fc9414828684dc24b864cca77f864e06bef3
yveskate07 <kateyveschadrac@gmail.com>
2026-04-19 20:34:16 +0000

    Commit on 2026-04-19 20:34:16, probleme avec le server de render. Test de la vue home.

--- AntaBackEnd/settings.py
+++ AntaBackEnd/settings.py
@@ -46,6 +46,28 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('SMTP_SERVER')
 EMAIL_PORT = config('SMTP_PORT')
