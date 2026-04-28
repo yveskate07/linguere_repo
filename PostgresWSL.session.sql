90f0d294326f8151713287da4d492d15c7edf4a3
yveskate07 <kateyveschadrac@gmail.com>
2026-04-28 14:20:49 +0000

    Commit on 2026-04-28 14:20:49, probleme avec le server de render. Test de la vue home.

--- PostgresWSL.session.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM public."Shop_product"
-ORDER BY id ASC 
--- anta_repo.sh
+++
@@ -1,4 +0,0 @@
-git remote remove origin
-git remote add origin https://github.com/linguerefablab/projet_site_linguere.git
-git rm -r --cached .
-./commit_and_push.sh
