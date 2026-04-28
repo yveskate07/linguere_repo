ac5f07c2c8c20118d9f0431cfb26ec0a4a1f4fdd
yveskate07 <kateyveschadrac@gmail.com>
2026-04-28 13:58:02 +0000

    Commit on 2026-04-28 13:58:02, probleme avec le server de render. Test de la vue home.

--- Testimonies/apps.py
+++ Testimonies/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TestimoniesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Testimonies'
+    verbose_name = "Témoignages"
