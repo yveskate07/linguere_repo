8074a6bc74c9b59b8b9b637d864a977a5a15e2a2
yveskate07 <kateyveschadrac@gmail.com>
2026-04-28 14:03:06 +0000

    Commit on 2026-04-28 14:03:06, probleme avec le server de render. Test de la vue home.

--- New_features/apps.py
+++ New_features/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class NewFeaturesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'New_features'
-    verbose_name="New features"
+    verbose_name="Nouvelles fonctionnalités"
--- Partners/apps.py
+++ Partners/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PartnersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Partners'
+    verbose_name = "Partenaires"
